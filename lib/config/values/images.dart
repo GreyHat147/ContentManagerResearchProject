9da1bdd85cc148a75e10346e4a941197799aac1e
Carlos Rojas <carlosandresrojasvalbuena521@gmail.com>
2023-09-13 19:51:18 -0500

    chore: add team photos to new folder

--- lib/config/values/images.dart
+++ lib/config/values/images.dart
@@ -11,4 +11,19 @@ class ImagePath {
   static const String blobSmallBeanAsh = "$imageDir/blob_small_bean_ash.png";
   static const String dotsGlobeYellow = "$imageDir/dots_globe_yellow.png";
   static const String dotsGlobeGrey = "$imageDir/dots_globe_grey.png";
+
+  //Team pics
+  static const String member1 = "$imageDir/images/member1.png";
+  static const String member2 = "$imageDir/images/member2.png";
+  static const String member3 = "$imageDir/images/member3.jpg";
+  static const String member4 = "$imageDir/images/member4.jpg";
+  static const String member5 = "$imageDir/images/member5.jpg";
+  static const String member6 = "$imageDir/images/member6.jpeg";
+  static const String member7 = "$imageDir/images/member7.jpg";
+  static const String member8 = "$imageDir/images/member8.jpg";
+  static const String member9 = "$imageDir/images/member9.jpg";
+  static const String member10 = "$imageDir/images/member10.jpg";
+  static const String member11 = "$imageDir/images/member11.jpg";
+  static const String member12 = "$imageDir/images/member12.jpg";
+  static const String member13 = "$imageDir/images/member13.jpeg";
 }
